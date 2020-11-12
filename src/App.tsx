276cbf025f3b3292df6ac1841483d05e0ca254e7
Nataliia Zevenko <nataliiazevenko@Nataliias-MacBook-Pro.local>
2020-11-11 16:50:08 -0800

    run simple title

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import WeekContainer from './components/WeekContainer';
+
+class App extends React.Component{
+  render(){
+    return (
+    <div className="App">
+      <WeekContainer />
+    </div>
+    )
+  }
+}
+
+export default App;
